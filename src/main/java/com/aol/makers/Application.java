cfe4bcf02663321414526af32bd97a25d53740fe
Jayaraman, Vaidyanathan <v.jayaraman@teamaol.com>
2015-06-26 18:04:20 -0400

    added extends SpringBootServletInitializer

--- src/main/java/com/aol/makers/Application.java
+++ src/main/java/com/aol/makers/Application.java
@@ -1,13 +1,14 @@
 package com.aol.makers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.aol.makers")
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
     	System.out.println("hello world");
         SpringApplication.run(Application.class, args);
     }
